aeea894c548b2c47f64a453438033c4170f0d0c5
pat-lyz <grace6lyz@gmail.com>
2025-11-11 09:19:00 -0700

    Update lab_5_top_level_students.sv

--- lab_5_top_level_students.sv
+++ lab_5_top_level_students.sv
@@ -60,54 +60,6 @@ module lab_5_top_level_students (
     .ready_pulse(ready_pulse)
     );
     
-    // XADC Instantiation
-    /*xadc_wiz_0 XADC_INST (
-        .di_in(16'h0000),        // Not used for reading
-        .daddr_in(CHANNEL_ADDR), // Channel address
-        .den_in(enable),         // Enable signal
-        .dwe_in(1'b0),           // Not writing, so set to 0
-        .drdy_out(ready),        // Data ready signal (when high, ADC data is valid)
-        .do_out(data),           // ADC data output
-        .dclk_in(clk),           // Use system clock
-        .reset_in(reset),   // Active-high reset
-        .vp_in(1'b0),            // Not used, leave disconnected
-        .vn_in(1'b0),            // Not used, leave disconnected
-        .vauxp15(vauxp15),       // Auxiliary analog input (positive)
-        .vauxn15(vauxn15),       // Auxiliary analog input (negative)
-        .channel_out(),          // Current channel being converted
-        .eoc_out(enable),        // End of conversion
-        .alarm_out(),            // Not used
-        .eos_out(eos_out),       // End of sequence
-        .busy_out(busy_out)      // XADC busy signal
-    );
-
-   averager  
-   #( .power(12), //2**N samples, default is 2**8 = 256 samples
-      .N(16)     // # of bits to take the average of
-    ) 
-   AVERAGER
-    ( .reset(reset),
-      .clk(clk),
-      .EN(ready_pulse),
-      .Din(data),
-      .Q(ave_data)
-    );
-    always_ff @(posedge clk) begin
-        if (reset) begin
-            scaled_adc_data <= 0;
-            scaled_adc_data_temp <= 0;
-        end
-        else if (ready_pulse) begin
-            // Calculation: This scales FFFFh to 270Fh (i.e. 9999d)
-            //    mVolts = ave_data/(2^16 - 1) * 9999 = ave_data * 0.152575
-            //    mVolts ~ ave_data * 1250/2^13 = (ave_data) * 1250 >> 13
-            // NOTE: The 7-seg display will display in millivolts, 
-            //       i.e. 9999 is 0.9999 V or 999.9 mV
-            //       place the decimal point in the correct place!
-            scaled_adc_data <= (ave_data*1250) >> 13; // was scaled_adc_data_temp
-            //scaled_adc_data <= scaled_adc_data_temp; // additional register faciliates pipelining
-        end                                          // for higher clock frequencies
-    end*/
 // Connect ADC data to LEDs
 assign led = scaled_adc_data;
     bin_to_bcd BIN2BCD (
@@ -165,3 +117,4 @@ assign led = scaled_adc_data;
     );
     
 endmodule
+
