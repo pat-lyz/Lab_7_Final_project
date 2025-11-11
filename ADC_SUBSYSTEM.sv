2a0efffdc578ce990c0340c9dc7043b964d8d02f
pat-lyz <grace6lyz@gmail.com>
2025-11-11 09:21:25 -0700

    Update ADC_SUBSYSTEM.sv

--- ADC_SUBSYSTEM.sv
+++ ADC_SUBSYSTEM.sv
@@ -83,7 +83,8 @@ module adc_subsystem(
             // NOTE: The 7-seg display will display in millivolts, 
             //       i.e. 9999 is 0.9999 V or 999.9 mV
             //       place the decimal point in the correct place!
-            scaled_adc_data <= (ave_data*4996) >> 15; // was scaled_adc_data_temp
+            // Convert averaged ADC → millivolts (0–3300 mV)
+            scaled_adc_data <= (((ave_data * 4996) >> 15) * 33) / 100;    // was scaled_adc_data_temp
             //scaled_adc_data <= scaled_adc_data_temp; // additional register faciliates pipelining
         end 
    
