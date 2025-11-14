eb2fef37b00b6013cc3f0204412c0368be54cbd7
pat-lyz <grace6lyz@gmail.com>
2025-11-14 16:49:23 -0700

    Update mux4_16_bits.sv

--- mux4_16_bits.sv
+++ mux4_16_bits.sv
@@ -1,29 +1,41 @@
 module mux4_16_bits(
-    input  logic [15:0] raw_adc,  
-    input  logic [15:0] averaged_adc,  
-    input  logic [15:0] scaled_voltage, 
+    input  logic [15:0] in0,  
+    input  logic [15:0] in1,  
+    input  logic [15:0] in2, 
     input  logic [15:0] in3,  
-    input  logic  [1:0] select,  
-    output logic [15:0] display_data,
+    input  logic [15:0] in4,
+    input  logic [15:0] in5,
+    input  logic [15:0] in6,
+    input  logic [15:0] in7,
+    input  logic  [2:0] select,  
+    output logic [15:0] mux_out,
     output logic  [3:0] decimal_point  
     );
 
     always_comb begin
         case(select)
-            2'b00: display_data = raw_adc;  // raw ADC
-            2'b01: display_data = averaged_adc;  // averaged ADC
-            2'b10: display_data = scaled_voltage;  // scaled voltage
-            2'b11: display_data = in3;  // unused
-            default: display_data = 16'h0000;  // Default case: output all zeros
+            3'b000: mux_out = in0;  
+            3'b001: mux_out = in1;  
+            3'b010: mux_out = in2;
+            3'b011: mux_out = in3;
+            3'b100: mux_out = in4;
+            3'b101: mux_out = in5;
+            3'b110: mux_out = in6;
+            3'b111: mux_out = in7;
+            default: mux_out = 16'h0000;  // Default case: output all zeros
         endcase
     end    
 
    always_comb begin
      case(select)
-         2'b00: decimal_point = 4'b0000;  // averaged ADC with extra 4 bits
-         2'b01: decimal_point = 4'b1000;  // averaged and scaled voltage
-         2'b10: decimal_point = 4'b0000;  // raw ADC (12-bits)
-         2'b11: decimal_point = 4'b0000;
+         3'b000: decimal_point = 4'b0000;  // averaged ADC with extra 4 bits
+         3'b001: decimal_point = 4'b1000;  // averaged and scaled voltage
+         3'b010: decimal_point = 4'b0000;  // raw ADC (12-bits)
+         3'b011: decimal_point = 4'b0000;
+         3'b100: decimal_point = 4'b0000;
+         3'b101: decimal_point = 4'b1000;
+         3'b110: decimal_point = 4'b0000;  
+         3'b111: decimal_point = 4'b0000;
          default: decimal_point = 16'h0000;  // Default case: output all zeros
      endcase
    end    
