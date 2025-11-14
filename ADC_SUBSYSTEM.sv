e3135c65332c07bf2fa55d2f05c01faa2b81b5c9
pat-lyz <grace6lyz@gmail.com>
2025-11-14 16:49:47 -0700

    Update ADC_SUBSYSTEM.sv

--- ADC_SUBSYSTEM.sv
+++ ADC_SUBSYSTEM.sv
@@ -30,14 +30,16 @@ module adc_subsystem(
     output logic [15:0] data,
     output logic [15:0] ave_data,
     output logic [15:0] scaled_adc_data,
+    output logic [15:0] bcd_value,
     output logic ready_pulse
     );
-
-    // Internal Signal declarations
-    localparam CHANNEL_ADDR = 7'h1f;      // XA4/AD15 (for XADC4)
-    logic        eos_out;                 // End of sequence          
-    logic        busy_out;                // XADC Busy signal
-    logic [15:0] scaled_adc_data_temp;    // scaled_adc_data, data;   
+    
+    localparam CHANNEL_ADDR = 7'h1f;     // XA4/AD15 (for XADC4)
+    //logic CHANNEL_ADDR;
+    logic        eos_out;
+    logic        busy_out;
+    logic [15:0] scaled_adc_data_temp;// scaled_adc_data, data;   
+    //logic        ready_pulse;
     
     xadc_wiz_0 XADC_INST (
         .di_in(16'h0000),        // Not used for reading
@@ -47,7 +49,7 @@ module adc_subsystem(
         .drdy_out(ready),        // Data ready signal (when high, ADC data is valid)
         .do_out(data),           // ADC data output
         .dclk_in(clk),           // Use system clock
-        .reset_in(reset),        // Active-high reset
+        .reset_in(reset),   // Active-high reset
         .vp_in(1'b0),            // Not used, leave disconnected
         .vn_in(1'b0),            // Not used, leave disconnected
         .vauxp15(vauxp15),       // Auxiliary analog input (positive)
@@ -69,28 +71,31 @@ module adc_subsystem(
       .EN(ready_pulse),
       .Din(data),
       .Q(ave_data)
-     
     );
+    
     always_ff @(posedge clk) begin
         if (reset) begin
             scaled_adc_data <= 0;
             scaled_adc_data_temp <= 0;
         end
         else if (ready_pulse) begin
-            // Calculation: This scales FFFFh to 0CE4h (i.e. 3300d)
-            //    mVolts = ave_data/(2^16 - 1) * 3300 = ave_data * 0.050355
-            //    mVolts ~ ave_data * 1650/2^15 = (ave_data) * 1250 >> 13
-            // NOTE: The 7-seg display will display in volts, 
-            //       i.e. 3300 is 3.300 V or 3.300 V
+            // Calculation: This scales FFFFh to 270Fh (i.e. 9999d)
+            //    mVolts = ave_data/(2^16 - 1) * 9999 = ave_data * 0.152575
+            //    mVolts ~ ave_data * 1250/2^13 = (ave_data) * 1250 >> 13
+            // NOTE: The 7-seg display will display in millivolts, 
+            //       i.e. 9999 is 0.9999 V or 999.9 mV
             //       place the decimal point in the correct place!
-            // Convert averaged ADC → volts (0–3300 mV)
-            scaled_adc_data <= (ave_data * 1650) >> 15;    // was scaled_adc_data_temp
+            scaled_adc_data <= (ave_data*1650) >> 15; // was scaled_adc_data_temp
             //scaled_adc_data <= scaled_adc_data_temp; // additional register faciliates pipelining
         end 
-   
-        
-                                      // for higher clock frequencies
-    end 
 
+    end 
+        
+    bin_to_bcd BIN_TO_BCD (
+        .clk(    clk),
+        .reset(  reset),
+        .bin_in( scaled_adc_data),
+        .bcd_out(bcd_value)
+    );
     
 endmodule
