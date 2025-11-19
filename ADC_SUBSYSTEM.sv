f33f10c47692b6f6904c42657751e91fa8f64f65
pat-lyz <grace6lyz@gmail.com>
2025-11-18 21:30:01 -0700

    Update ADC_SUBSYSTEM.sv
    Updated comments and all that

--- ADC_SUBSYSTEM.sv
+++ ADC_SUBSYSTEM.sv
@@ -1,23 +1,9 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/17/2025 11:41:07 AM
-// Design Name: 
-// Module Name: ADC_SUBSYSTEM
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+/*
+This module runs the XADC wizard which samples a voltage taken from the vanxp15 and vanxn15 pins
+outputting the raw data, the averaged data, and the scaled and averaged data in both Hexidecimal
+and decimal format.
+*/
 
 
 module adc_subsystem(
@@ -27,20 +13,19 @@ module adc_subsystem(
     input logic reset,    
     output logic ready,        
     output logic enable,
-    output logic [15:0] data,
-    output logic [15:0] ave_data,
-    output logic [15:0] scaled_adc_data,
-    output logic [15:0] bcd_value,
-    output logic ready_pulse
+    output logic [15:0] data,               //raw data, in hexidecimal
+    output logic [15:0] ave_data,           //averaged data, in hexidecimal
+    output logic [15:0] scaled_adc_data,    //scaled data in hexidecimal
+    output logic [15:0] bcd_value           //scaled data in decimal
     );
     
-    localparam CHANNEL_ADDR = 7'h1f;     // XA4/AD15 (for XADC4)
-    //logic CHANNEL_ADDR;
-    logic        eos_out;
-    logic        busy_out;
-    logic [15:0] scaled_adc_data_temp;// scaled_adc_data, data;   
-    //logic        ready_pulse;
-    
+    logic        ready_r, ready_pulse;
+    localparam CHANNEL_ADDR = 7'h1f;    // XA4/AD15 (for XADC4)
+    logic        eos_out;               //end of sequence
+    logic        busy_out;              // bisy signal
+    logic [15:0] scaled_adc_data_temp;  // scaled_adc_data, data;   
+
+    //instantiates the XADC wizard form the IP Catalog
     xadc_wiz_0 XADC_INST (
         .di_in(16'h0000),        // Not used for reading
         .daddr_in(CHANNEL_ADDR), // Channel address
@@ -60,7 +45,8 @@ module adc_subsystem(
         .eos_out(eos_out),       // End of sequence
         .busy_out(busy_out)      // XADC busy signal
     );
-      
+     
+    //averages the raw data  
        averager  
    #( .power(12), //2**N samples, default is 2**8 = 256 samples
       .N(16)     // # of bits to take the average of
@@ -73,29 +59,42 @@ module adc_subsystem(
       .Q(ave_data)
     );
     
+    //scales the averaged data
     always_ff @(posedge clk) begin
         if (reset) begin
             scaled_adc_data <= 0;
             scaled_adc_data_temp <= 0;
         end
         else if (ready_pulse) begin
-            // Calculation: This scales FFFFh to 270Fh (i.e. 9999d)
-            //    mVolts = ave_data/(2^16 - 1) * 9999 = ave_data * 0.152575
-            //    mVolts ~ ave_data * 1250/2^13 = (ave_data) * 1250 >> 13
-            // NOTE: The 7-seg display will display in millivolts, 
-            //       i.e. 9999 is 0.9999 V or 999.9 mV
+            // Calculation: This scales FFFFh to 0CE4h (i.e. 3300d)
+            //    mVolts = ave_data/(2^16 - 1) * 3300 = ave_data * 0.050355
+            //    mVolts ~ ave_data * 1650/2^15 = (ave_data) * 1250 >> 13
+            // NOTE: The 7-seg display will display in volts, 
+            //       i.e. 3300 is 3.300 V or 3.300 V
             //       place the decimal point in the correct place!
+            // Convert averaged ADC → volts (0-3300 mV)
             scaled_adc_data <= (ave_data*1650) >> 15; // was scaled_adc_data_temp
             //scaled_adc_data <= scaled_adc_data_temp; // additional register faciliates pipelining
         end 
 
     end 
-        
+    
+    //converts the scaled_adc_value to decimal form    
     bin_to_bcd BIN_TO_BCD (
         .clk(    clk),
         .reset(  reset),
         .bin_in( scaled_adc_data),
         .bcd_out(bcd_value)
     );
+    
+    // added these 3 lines for the pulser 
+  
+  always_ff@(posedge clk)
+    if (reset)
+       ready_r <= 0;
+    else
+       ready_r <= ready;
+       
+  assign ready_pulse = ~ready_r & ready; // generate 1-clk pulse when ready goes high
     
 endmodule
